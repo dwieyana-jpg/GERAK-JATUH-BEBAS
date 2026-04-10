8f04b8999709332a54c7aecd2dcf82b9f2c4a54c
dwieyana-jpg <dwieyana@gmail.com>
2026-04-10 08:53:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,16 @@ resizeCanvas();
 // PARAMETER FISIKA
 // =====================
 const g = 9.8;
-const m = 1;
-
+let m = 1;
 let h0 = 10;
+
 let y, v, t;
 
 let running = false;
 let paused = false;
+
+// jejak lintasan
+let trail = [];
 
 // load image
 const ballImg = new Image();
@@ -31,13 +34,22 @@ ballImg.src = "ball.png";
 // =====================
 // SLIDER
 // =====================
-const slider = document.getElementById("heightSlider");
+const heightSlider = document.getElementById("heightSlider");
 const hValue = document.getElementById("hValue");
 
-slider.oninput = function () {
+heightSlider.oninput = function () {
   h0 = parseFloat(this.value);
   hValue.innerText = h0;
   resetSim();
+};
+
+const massSlider = document.getElementById("massSlider");
+const mValue = document.getElementById("mValue");
+
+massSlider.oninput = function () {
+  m = parseFloat(this.value);
+  mValue.innerText = m;
+  updateOutput();
 };
 
 // =====================
@@ -59,6 +71,7 @@ function resetSim() {
   t = 0;
   v = 0;
   y = h0;
+  trail = [];
   draw();
   updateOutput();
 }
@@ -77,6 +90,9 @@ function update(dt) {
     y = 0;
     running = false;
   }
+
+  // simpan jejak lintasan
+  trail.push(y);
 
   updateOutput();
 }
@@ -106,11 +122,29 @@ function draw() {
   ctx.fillStyle = "#4CAF50";
   ctx.fillRect(0, H - 20, W, 20);
 
-  // konversi skala
   let scale = (H - 40) / h0;
+
+  // ====== GARIS LINTASAN (PUTUS-PUTUS) ======
+  ctx.setLineDash([5, 5]);
+  ctx.beginPath();
+
+  trail.forEach((yy, i) => {
+    let yPixel = H - 20 - yy * scale;
+    if (i === 0) {
+      ctx.moveTo(W / 2, yPixel);
+    } else {
+      ctx.lineTo(W / 2, yPixel);
+    }
+  });
+
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.setLineDash([]);
+
+  // posisi bola
   let yPixel = H - 20 - y * scale;
 
-  // bola
   ctx.drawImage(ballImg, W / 2 - 20, yPixel - 20, 40, 40);
 }
 
